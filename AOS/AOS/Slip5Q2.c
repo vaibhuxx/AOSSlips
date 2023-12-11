48dfbb1a6da1f8519fb1ba39a0b3dae3af998597
scooby <78257652+vaibhuxx@users.noreply.github.com>
2023-12-12 00:17:50 +0530

    Update Slip5Q2.c

--- AOS/AOS/Slip5Q2.c
+++ AOS/AOS/Slip5Q2.c
@@ -1,68 +1,47 @@
-/*Write a C program to create an unnamed pipe. The child process will write following
-three messages to pipe and parent process display it.
-Message1 = “Hello World”
-Message2 = “Hello SPPU”
-Message3 = “Linux is Funny”*/
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-#define MESSAGE1 "Hello World"
-#define MESSAGE2 "Hello SPPU"
-#define MESSAGE3 "Linux is Funny"
-
 int main() {
-    int pipe_fd[2];
-    pid_t child_pid;
+  int pipefds[2]; // Array to store read and write file descriptors
+  int return_status; // Variable to store return values
+  char message1[] = "Hello World";
+  char message2[] = "Hello SPPU";
+  char message3[] = "Linux is Funny";
+  char read_message[50]; // Buffer to read messages
 
-    // Create a pipe
-    if (pipe(pipe_fd) == -1) {
-        perror("Error creating pipe");
-        exit(EXIT_FAILURE);
-    }
+  // Create an unnamed pipe
+  return_status = pipe(pipefds);
+  if (return_status == -1) {
+    printf("Error creating pipe\n");
+    return 1;
+  }
 
-    // Create a child process
-    if ((child_pid = fork()) == -1) {
-        perror("Error creating child process");
-        exit(EXIT_FAILURE);
-    }
+  // Fork a child process
+  int child_pid = fork();
+  if (child_pid == 0) { // Child process
+    printf("Child is writing to pipe:\n");
 
-    if (child_pid == 0) {
-        // Child process
+    // Write messages to the pipe
+    write(pipefds[1], message1, sizeof(message1));
+    printf("Message 1: %s\n", message1);
 
-        // Close the read end of the pipe
-        close(pipe_fd[0]);
+    write(pipefds[1], message2, sizeof(message2));
+    printf("Message 2: %s\n", message2);
+    write(pipefds[1], message3, sizeof(message3));
+    printf("Message 3: %s\n", message3);
+  } else if (child_pid > 0) { // Parent process
+    // Read messages from the pipe
+    read(pipefds[0], read_message, sizeof(read_message));
+    printf("Parent Process is Reading from pipe: %s\n", read_message);
 
-        // Write three messages to the pipe
-        write(pipe_fd[1], MESSAGE1, sizeof(MESSAGE1));
-        write(pipe_fd[1], MESSAGE2, sizeof(MESSAGE2));
-        write(pipe_fd[1], MESSAGE3, sizeof(MESSAGE3));
+    read(pipefds[0], read_message, sizeof(read_message));
+    printf("Parent Process is Reading from pipe: %s\n", read_message);
 
-        // Close the write end of the pipe
-        close(pipe_fd[1]);
-
-        exit(EXIT_SUCCESS);
-    } else {
-        // Parent process
-
-        // Close the write end of the pipe
-        close(pipe_fd[1]);
-
-        // Read and display messages from the pipe
-        char buffer[256];
-        int bytesRead;
-
-        while ((bytesRead = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) {
-            printf("Parent Process: Received message: %.*s\n", bytesRead, buffer);
-        }
-
-        // Close the read end of the pipe
-        close(pipe_fd[0]);
-
-        // Wait for the child process to finish
-        wait(NULL);
-    }
-
-    return 0;
+    ssize_t data_len = read(pipefds[0], read_message, sizeof(read_message));
+    printf("Parent Process is Reading from pipe: %s\n", read_message);
+  } else { // Fork error
+    printf("Error forking child process\n");
+    return 1;
+  }
+  return 0;
 }
